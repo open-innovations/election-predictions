acd282df59a36f06aeb7d781ea42f0efd3cb5343
Giles Dring <giles@dringtech.com>
2024-06-25 11:04:57 +0100

    Integrate lume charts and helpers

--- _config.ts
+++ _config.ts
@@ -1,8 +1,16 @@
 import lume from "lume/mod.ts";
 import base_path from "lume/plugins/base_path.ts";
+import favicon from "lume/plugins/favicon.ts";
 import metas from "lume/plugins/metas.ts";
 import postcss from "lume/plugins/postcss.ts";
-import favicon from "lume/plugins/favicon.ts";
+import sheets from "lume/plugins/sheets.ts";
+
+import oiLumeViz from "https://deno.land/x/oi_lume_viz@v0.15.6/mod.ts";
+import { names } from "./colour.ts";
+
+import autoDependency from "https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts";
+import json from "lume/core/loaders/json.ts";
+
 const site = lume({
   src: "./src",
   // TODO Update this with the proper URL
@@ -14,6 +22,15 @@ site.use(postcss());
 site.use(favicon({
   input: "assets/images/oi-logo.svg",
 }));
+site.use(sheets());
+site.use(oiLumeViz({
+  colour: {
+    names,
+  },
+}));
+site.process([".html"], (pages) => pages.forEach(autoDependency));
+
+site.loadData([".hexjson"], json);
 site.use(base_path());
 
 site.remoteFile(
---
+++ colour.ts
@@ -0,0 +1,11 @@
+// TODO Put the proper colours in here.
+export const names = {
+  lab: "#ff0000",
+  con: "blue",
+  green: "#00ff00",
+  ld: "yellow",
+  other: "#aaa",
+  pc: "cyan",
+  ref: "pink",
+  snp: "orange",
+};
