aac8e5a084bad3d506ea6a68a564aef6f361a424
Giles Dring <giles@dringtech.com>
2024-06-25 10:42:04 +0100

    Tidying up code with deno fmt

--- _config.ts
+++ _config.ts
@@ -4,7 +4,7 @@ import metas from "lume/plugins/metas.ts";
 import postcss from "lume/plugins/postcss.ts";
 import favicon from "lume/plugins/favicon.ts";
 const site = lume({
-  src: './src',
+  src: "./src",
   // TODO Update this with the proper URL
   location: new URL("https://open-innovations.github.io/election-predictions/"),
 });
@@ -12,13 +12,13 @@ const site = lume({
 site.use(metas());
 site.use(postcss());
 site.use(favicon({
-  input: "assets/images/oi-logo.svg"
+  input: "assets/images/oi-logo.svg",
 }));
 site.use(base_path());
 
-site.remoteFile('assets/images/oi-logo.svg',
-"https://open-innovations.org/resources/images/logos/oi-square-black.svg"
-)
-
+site.remoteFile(
+  "assets/images/oi-logo.svg",
+  "https://open-innovations.org/resources/images/logos/oi-square-black.svg",
+);
 
 export default site;
