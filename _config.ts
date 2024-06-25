89b341d937346b6db0ca4b84c4f9456e598aceb9
Giles Dring <giles@dringtech.com>
2024-06-25 10:39:27 +0100

    Slimming down lume config

--- _config.ts
+++ _config.ts
@@ -1,23 +1,24 @@
 import lume from "lume/mod.ts";
-import nunjucks from "lume/plugins/nunjucks.ts";	// Lume 2.0 requires us to add Nunjucks
 import base_path from "lume/plugins/base_path.ts";
-import date from "lume/plugins/date.ts";
 import metas from "lume/plugins/metas.ts";
 import postcss from "lume/plugins/postcss.ts";
-
+import favicon from "lume/plugins/favicon.ts";
 const site = lume({
   src: './src',
   // TODO Update this with the proper URL
-  location: new URL("https://open-innovations.github.io/oi-lume-template/"),
+  location: new URL("https://open-innovations.github.io/election-predictions/"),
 });
-site.use(nunjucks()); // For Lume 2
-site.use(base_path());
-site.use(metas({
-  defaultPageData: {
-    title: 'title', // Use the `date` value as fallback.
-  },
+
+site.use(metas());
+site.use(postcss());
+site.use(favicon({
+  input: "assets/images/oi-logo.svg"
 }));
-site.use(date());
-site.use(postcss({}));
+site.use(base_path());
+
+site.remoteFile('assets/images/oi-logo.svg',
+"https://open-innovations.org/resources/images/logos/oi-square-black.svg"
+)
+
 
 export default site;
